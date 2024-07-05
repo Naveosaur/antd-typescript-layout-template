7bfa16765a9eba16ac80f8f089f02d031496fe66
Naveoo <84273067+Naveosaur@users.noreply.github.com>
2024-07-05 11:22:05 +0700

    Feat: Login Middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,32 @@
-// Middleware for Authorization
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  if (!request.cookies.has("token")) {
-    return NextResponse.redirect(new URL("/login", request.url));
+  const token = request.cookies.get("token");
+
+  // If the user is not logged in and tries to access any page, redirect to login
+  if (!token) {
+    if (request.nextUrl.pathname !== "/login") {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
   }
-  NextResponse.next();
+
+  // If the user is logged in and tries to access the root, redirect to dashboard
+  if (token) {
+    if (request.nextUrl.pathname === "/") {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+
+    // If the user is logged in and tries to access the login page, redirect to dashboard
+    if (request.nextUrl.pathname === "/login") {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+  }
+
+  // Continue with the request
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/", "/dashboard/:path*"],
+  matcher: ["/", "/dashboard/:path*", "/login"],
 };
